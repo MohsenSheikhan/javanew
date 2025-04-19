ce1cd876dc1d3f44946af9f8895f48fb2b345449
mohsen <sheikhancs@gmail.com>
2025-04-19 22:51:00 +0330

    initial commit

--- src/main/java/com/mycompany/mohsenproject/MohsenProject.java
+++ src/main/java/com/mycompany/mohsenproject/MohsenProject.java
@@ -11,6 +11,6 @@ package com.mycompany.mohsenproject;
 public class MohsenProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello1 World!");
+        System.out.println("Hello1 opWorld!");
     }
 }
